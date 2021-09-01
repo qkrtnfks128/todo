3ef07c052d90246f9e2fc43dfb9852cdaf389a32
박망키 <sulan6789@naver.com>
2021-09-01 17:00:50 +0900

    21.9.1

--- src/pages/main/D3.vue
+++ src/pages/main/D3.vue
@@ -25,42 +25,53 @@ export default {
     },
     methods: {
         generateAxis() {
-        const xScale = d3
-            .scaleTime()
-            .domain([
-                new Date("2020-07-01T03:00:00"),
-                new Date("2020-07-01T03:25:00")
-            ])
-            .range([20, 330]);
+            
+            const xScale = d3
+                .scaleTime()
+                .domain([
+                    new Date("2020-07-01T03:00:00"),
+                    new Date("2020-07-01T03:25:00")
+                ])
+                .range([20, 330]);
+                
 
-        const yScale = d3
-            .scaleLinear()
-            .domain([0, 200])
-            .range([330, 20]); // SVG 좌표상에서 y값이 높을수록 아래로 향하기 때문에 뒤집어서 330~20으로 설정.
+            const yScale = d3
+                .scaleLinear()
+                .domain([0, 200])
+                .range([330, 20]); // SVG 좌표상에서 y값이 높을수록 아래로 향하기 때문에 뒤집어서 330~20으로 설정.
 
-        const xAxisSVG = d3.select("svg").append("g").attr("transform", "translate(0,330)"); //attr속성을 이용하면 선택한 태그들의 속성을 바꿀 수 있다.
-        const yAxisSVG = d3.select("svg").append("g");
+            const xAxisSVG = d3.select("svg").append("g").attr("transform", "translate(0,330)"); //attr속성을 이용하면 선택한 태그들의 속성을 바꿀 수 있다.
+            const yAxisSVG = d3.select("svg").append("g");
 
-        //축을 만드는 함수를 만든다.
-        const xAxis = d3
-            .axisBottom(xScale)
-            .tickSize(10)
-            .ticks(10); // 여기서 tick은 축마다 달려있는 막대기를 말한다.
-        const yAxis = d3
-            .axisRight(yScale)
-            .tickSize(10)
-            .ticks(10);
-        xAxis(xAxisSVG); //x축을 만드는 함수로 SVG > G 태그에 축을 생성한다.
-        yAxis(yAxisSVG); //y축을 만드는 함수로 SVG > G 태그에 축을 생성한다.
-        d3.select("svg").selectAll("circle")  	  // 1.SVG 태그 안에 있는 circle을 모두 찾는다.
-            .data(this.data)                         // 2.찾은 요소에 데이터를 씌운다.
-            .enter()                            // 3.찾은 요소에 개수보다 데이터가 더 많을경우..
-            .append("circle")                   // 4.circle 을 추가한다.
-            .attr("r", 5)                       //  - 반지름 5픽셀
-            .attr("cx", d=>xScale(d.time))      //  - x 위치값 설정.
-            .attr("cy", d=>yScale(d.value))     //  - y 위치값 설정. 
-            .style("fill", "black")             //  - 검정색
+            //축을 만드는 함수를 만든다.
+            const xAxis = d3
+                .axisBottom(xScale)
+                .tickSize(10)
+                .ticks(10); // 여기서 tick은 축마다 달려있는 막대기를 말한다.
+            const yAxis = d3
+                .axisRight(yScale)
+                .tickSize(10)
+                .ticks(10);
+            xAxis(xAxisSVG); //x축을 만드는 함수로 SVG > G 태그에 축을 생성한다.
+            yAxis(yAxisSVG); //y축을 만드는 함수로 SVG > G 태그에 축을 생성한다.
+            d3.select("svg").selectAll("circle")  	  // 1.SVG 태그 안에 있는 circle을 모두 찾는다.
+                .data(this.data)                         // 2.찾은 요소에 데이터를 씌운다.
+                .enter()                            // 3.찾은 요소에 개수보다 데이터가 더 많을경우..
+                .append("circle")                   // 4.circle 을 추가한다.
+                .attr("r", 5)                       //  - 반지름 5픽셀
+                .attr("cx", d=>xScale(d.time))      //  - x 위치값 설정.
+                .attr("cy", d=>yScale(d.value))     //  - y 위치값 설정. 
+                
+                .style("fill", "black")             //  - 검정색
+                
+                var newX = d3.transform.rescaleX(x);
+                var newY = d3.transform.rescaleY(y);
+                
+                xAxis.call(d3.axisBottom(newX))
+                yAxis.call(d3.axisLeft(newY))
+
             }
+
     }
 };
 </script>
--- src/pages/main/D3chart.vue
+++ src/pages/main/D3chart.vue
@@ -1,17 +1,78 @@
 <template>
     <div>
         <h3>d3chart</h3>
+        <div id="dataviz_basicZoom"></div>
         
     </div>
 </template>
 
 <script>
-
+import * as d3 from "d3";
 export default {
+    mounted(){
+        this.create();
+        this.linechart();
+    },
+    methods:{
+        create(){
+            var svg = d3.select("#dataviz_basicZoom")
+            .append("svg")
+            .attr("width",  300)
+            .attr("height",  300)
+            .call(d3.zoom().on("zoom", function({transform}) {
+                svg.attr("transform", transform)
+            }))
+            .append("g")
 
+            svg
+            .append("circle")
+            .attr("cx", 100)
+            .attr("cy", 100)
+            .attr("r", 40)
+            .style("fill", "white")
+            
+            svg
+            .append("circle")
+            .attr("cx", 100)
+            .attr("cy", 100)
+            .attr("r", 30)
+            .style("fill", "black")
+
+
+            svg
+            .append("circle")
+            .attr("cx", 200)
+            .attr("cy", 100)
+            .attr("r", 40)
+            .style("fill", "white")
+
+            svg
+            .append("circle")
+            .attr("cx", 200)
+            .attr("cy", 100)
+            .attr("r", 30)
+            .style("fill", "black")
+
+            svg
+            .append("circle")
+            .attr("cx", 150)
+            .attr("cy", 150)
+            .attr("r", 30)
+            .style("fill", "brown")
+        },
+        linechart(){
+            
+            
+
+
+        }
+    }
 }
 </script>
 
 <style>
-
+#dataviz_basicZoom>svg{
+    background-color: #e2db99;
+    cursor:move;
+}
 </style>
